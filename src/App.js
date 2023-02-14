0b07f867f9df5c4cd10835131063505a8c125621
arun <arun.krishnan@byjus.com>
2023-02-14 14:53:28 +0530

    a site which shows movie details

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+//1044cf91
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=1044cf91";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const movie1 = {
+    Title: "Whiplash",
+    Year: "2014",
+    imdbID: "tt2582802",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTA5NDZlZGUtMjAxOS00YTRkLTkwYmMtYWQ0NWEwZDZiNjEzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  };
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Moobies</h1>
+
+      <div className="search">
+        <input placeholder="search here mofo" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+
+      {movies?.length > 0 ?    
+      (
+      <div className="container">
+        {movies.map((movie) => (
+            <MovieCard movie={movie} />
+            ))}
+      </div>
+      )
+      :
+      (
+        <div className="empty">
+          <h1>Tf are you searching?</h1>
+        </div>
+      )
+      }   
+
     </div>
   );
-}
+};
 
 export default App;
